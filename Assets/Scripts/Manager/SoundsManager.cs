4ae34c104c3f174c1c0bc6d35ad688380eeee640
Nay <ilan.outyey@gmail.com>
2023-11-24 16:05:14 +0100

    Sort files

---
+++ Assets/Scripts/Manager/SoundsManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsManager : MonoBehaviour
+{
+    public static SoundsManager instance;
+
+    AudioSource audioData;
+    public AudioClip jumpSound;
+    public AudioClip slideSound;
+    public AudioClip deathSound;
+
+    public int volume = 10;
+
+    // Ajoutez une variable pour suivre le dernier son joué
+    private AudioClip lastPlayedSound;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("Il existe déjà une instance de SoundsManager dans cette scène...");
+            return;
+        }
+        instance = this;
+
+        audioData = GetComponent<AudioSource>();
+    }
+
+    public void playSound(AudioClip sound)
+    {
+        // Vérifiez si le son est différent du dernier son joué
+        if ((sound != lastPlayedSound || !audioData.isPlaying) && lastPlayedSound != deathSound)
+        {
+            audioData.PlayOneShot(sound, volume);
+            lastPlayedSound = sound;
+        }
+        else
+        {
+            Debug.Log("Le son ne peut pas être joué actuellement.");
+        }
+    }
+
+    public void playJumpSound()
+    {
+        playSound(jumpSound);
+    }
+
+    public void playSlideSound()
+    {
+        playSound(slideSound);
+    }
+
+    public void playDeadSound()
+    {
+        playSound(deathSound);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+namespace SupanthaPaul
+{
+	public class PlayerAnimator : MonoBehaviour
+	{
+		private Rigidbody2D m_rb;
+		private PlayerController m_controller;
+		private Animator m_anim;
+		private DyingSystem dyingSystem;
+		private static readonly int Move = Animator.StringToHash("Move");
+		private static readonly int JumpState = Animator.StringToHash("JumpState");
+		private static readonly int IsJumping = Animator.StringToHash("IsJumping");
+		private static readonly int WallGrabbing = Animator.StringToHash("WallGrabbing");
+		private static readonly int IsSliding = Animator.StringToHash("IsSliding");
+		private static readonly int IsDying = Animator.StringToHash("IsDying");
+
+
+		private void Start()
+		{
+			m_anim = GetComponentInChildren<Animator>();
+			m_controller = GetComponent<PlayerController>();
+			dyingSystem = GetComponent<DyingSystem>();
+			m_rb = GetComponent<Rigidbody2D>();
+		}
+
+		private void Update()
+		{
+			// Idle & Running animation
+			m_anim.SetFloat(Move, Mathf.Abs(m_rb.velocity.x));
+
+			// Jump state (handles transitions to falling/jumping)
+			float verticalVelocity = m_rb.velocity.y;
+			m_anim.SetFloat(JumpState, verticalVelocity);
+
+			// Jump animation
+
+			if (!m_controller.isGrounded)
+			{
+				m_anim.SetBool(IsJumping, true);
+				SoundsManager.instance.playJumpSound();
+				SoundsManager.instance.playJumpSound();
+			}
+			else
+			{
+				m_anim.SetBool(IsJumping, false);
+			}
+			
+			m_anim.SetBool(IsSliding, m_controller.isSliding);
+			if (m_controller.isSliding){
+				SoundsManager.instance.playSlideSound();
+			}
+
+
+			m_anim.SetBool(IsDying, DyingSystem.instance.isDead);
+			if (DyingSystem.instance.isDead){
+				SoundsManager.instance.playDeadSound();
+			}
+		}
+
+		public float GetSlideAnimationDuration(string animationName)
+		{
+			// Récupérer l'état de l'animation par son nom
+			AnimatorStateInfo state = m_anim.GetCurrentAnimatorStateInfo(0);
+
+			// Vérifier si l'animation spécifiée est en cours
+			if (state.IsName(animationName))
+			{
+				// Retourner la durée totale de l'animation
+				return state.length;
+			}
+			else
+			{
+				// Retourner une valeur par défaut si l'animation n'est pas en cours
+				return 0f;
+			}
+		}
+	}
+}
