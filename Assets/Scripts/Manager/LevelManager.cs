654484e204b607183038807d0b9e4a5074602d56
Nay <ilan.outyey@gmail.com>
2023-12-04 14:02:25 +0100

    Squashed commit of the following:
    commit 21fc848510fa271102798c2259b2a7f1e379d364
    Author: Nay <ilan.outyey@gmail.com>
    Date:   Mon Dec 4 14:02:08 2023 +0100
    
        Merge Emmanuel to Ilan
    
    commit 2de1c56726516851fca86e438b7b499f0bc92e35
    Merge: 87f560e 78e7ae2
    Author: Nay <ilan.outyey@gmail.com>
    Date:   Mon Dec 4 13:52:04 2023 +0100
    
        Merge branch 'Emmanuel' into Ilan
    
    commit 87f560e33f0a040709f30223c87cd377d8beedb2
    Author: Nay <ilan.outyey@gmail.com>
    Date:   Mon Dec 4 13:36:18 2023 +0100
    
        commit
    
    commit d25428f0577fd2a6d66b9c30985d5d965d42eaac
    Author: Nay <ilan.outyey@gmail.com>
    Date:   Mon Dec 4 10:53:40 2023 +0100
    
        level manager states system
    
    commit a6f84061c8db544d15d427834f218bf15f9b2322
    Author: Nay <ilan.outyey@gmail.com>
    Date:   Mon Dec 4 08:53:59 2023 +0100
    
        Merge Emmanuel
    
    commit 9e6203406629ceef0938dc730f7df5ae4d756438
    Merge: d85593d 373e765
    Author: Nay <ilan.outyey@gmail.com>
    Date:   Mon Dec 4 08:53:43 2023 +0100
    
        Merge branch 'Emmanuel' into Ilan
    
    commit d85593d939e8e5edd6fdfef894b8d1936e791acc
    Author: Nay <ilan.outyey@gmail.com>
    Date:   Mon Dec 4 08:45:59 2023 +0100
    
        layers prefab
    
    commit e56b608c8c24b30875bce00831cbe19dbcc5c7c1
    Author: Kibishi47 <78955048+Kibishi47@users.noreply.github.com>
    Date:   Wed Nov 29 21:43:42 2023 +0100
    
        Update .gitignore

--- Assets/Scripts/Manager/LevelManager.cs
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
-
 
 public class LevelManager : MonoBehaviour
 {
+    public enum GameState
+    {
+        InLevel,
+        BetweenLevels
+    }
+
+    public GameState currentState = GameState.InLevel;
+
     private string firstDefaultTextIndicator = "Changement de mouvement dans ";
     private string secondDefaultTextIndicator = "...";
+
     [HideInInspector] public GameObject GO_parentChangeLevelBar;
     public int currentLevel = 1;
-    public int deltaScoreBeforeChangeLevel;
+    public int scoreToChangeLevel = 10;
     private int nextScoreBeforeChangeLevel;
     private int currentStepLevel = 0;
 
@@ -33,34 +40,49 @@ public class LevelManager : MonoBehaviour
 
     public string[] movementList;
     public string[] actionList;
-    
+
     public static LevelManager instance;
+
 
     [Header("Génération")]
     public ObstaclesFactory obstaclesFactory;
+
     void Awake()
     {
         GO_parentChangeLevelBar = changeLevelBar.transform.parent.gameObject;
 
         if (instance)
         {
-            Debug.Log("IL existe déjà une instance de LevelManager dans cette scène");
+            Debug.Log("Il existe déjà une instance de LevelManager dans cette scène");
             return;
         }
         instance = this;
     }
+    
 
     void Start()
     {
-        nextScoreBeforeChangeLevel = deltaScoreBeforeChangeLevel;
-
+        nextScoreBeforeChangeLevel = scoreToChangeLevel;
         changeLevelBar.SetNewValues(ScoreManager.instance.score, nextScoreBeforeChangeLevel);
-
         ChangeActionUX();
         RefreshTextLevel();
     }
-    
+
     void Update()
+    {
+        switch (currentState)
+        {
+            case GameState.InLevel:
+                UpdateInLevel();
+                break;
+
+            case GameState.BetweenLevels:
+                UpdateBetweenLevels();
+                break;
+        }
+    }
+
+    public void UpdateInLevel()
     {
         changeLevelBar.SetValue(ScoreManager.instance.score);
         int numberBeforeChangeLevel = nextScoreBeforeChangeLevel - ScoreManager.instance.score;
@@ -68,21 +90,32 @@ public class LevelManager : MonoBehaviour
         {
             PrintTextIndicator(true, numberBeforeChangeLevel);
         }
-        // Si on doit changer de level
-        if(ScoreManager.instance.score >= nextScoreBeforeChangeLevel)
-        {
-            if (currentStepLevel % 2 == 0){
-                currentLevel++;
-                obstaclesFactory.isGenerationEnabled = !obstaclesFactory.isGenerationEnabled;
 
-                RefreshTextLevel();
-                changeLevelBar.SetNewValues(ScoreManager.instance.score, nextScoreBeforeChangeLevel);
-                ChangeMovement();
-            }
-            nextScoreBeforeChangeLevel += deltaScoreBeforeChangeLevel;
+        if (ScoreManager.instance.score >= nextScoreBeforeChangeLevel)
+        {
+            currentLevel++;
             currentStepLevel++;
+            obstaclesFactory.isGenerationEnabled = !obstaclesFactory.isGenerationEnabled;
+
+            RefreshTextLevel();
+            changeLevelBar.SetNewValues(ScoreManager.instance.score, nextScoreBeforeChangeLevel);
+            ChangeMovement();
+            
+            nextScoreBeforeChangeLevel += scoreToChangeLevel;
+
+            currentState = GameState.BetweenLevels;
         }
     }
+
+    void UpdateBetweenLevels()
+    {
+        // Code spécifique pour la période entre les niveaux
+        // Vous pouvez désactiver les obstacles, etc.
+
+        currentStepLevel++;
+        currentState = GameState.InLevel; // Passe à l'état suivant lorsque nécessaire
+    }
+
 
     void ChangeMovement()
     {
@@ -102,7 +135,7 @@ public class LevelManager : MonoBehaviour
         leftActionUX.PrintImage(leftMovement);
         rightActionUX.PrintImage(rightMovement);
     }
-    
+
     void PrintTextIndicator(bool enabled, int number = 0)
     {
         GO_parentChangeLevelBar.SetActive(false);
