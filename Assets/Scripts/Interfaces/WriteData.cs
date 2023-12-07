8f20a1dcbc71e0f51722af0ebce25fcf70975c34
Kibishi47 <78955048+Kibishi47@users.noreply.github.com>
2023-12-07 14:10:58 +0100

    Animation End Data

--- Assets/Scripts/Interfaces/WriteData.cs
+++ Assets/Scripts/Interfaces/WriteData.cs
@@ -6,6 +6,7 @@ public class WriteData : MonoBehaviour
 {
     [Header("Data smooth animation")]
     public float timeAnimation;
+    public float timePrinting; 
     public float edge0;
     public float edge1;
 
@@ -14,7 +15,9 @@ public class WriteData : MonoBehaviour
     public Vector2 caloriesTargetSize;
     public float caloriesTargetRotation;
     public float textCaloriesDescFontSizeTarget;
+    public Vector2 textDescCaloriesTargetSize;
     public float textCaloriesFontSizeTarget;
+    public Vector2 textCaloriesTargetSize;
     public GameObject calories;
     public TextMeshProUGUI textCaloriesDesc;
     public TextMeshProUGUI textCalories;
@@ -24,7 +27,9 @@ public class WriteData : MonoBehaviour
     public Vector2 scoreTargetSize;
     public float scoreTargetRotation;
     public float textScoreDescFontSizeTarget;
+    public Vector2 textDescScoreTargetSize;
     public float textScoreFontSizeTarget;
+    public Vector2 textScoreTargetSize;
     public GameObject score;
     public TextMeshProUGUI textScoreDesc;
     public TextMeshProUGUI textScore;
@@ -34,7 +39,9 @@ public class WriteData : MonoBehaviour
     public Vector2 obstaclesTargetSize;
     public float obstaclesTargetRotation;
     public float textObstaclesDescFontSizeTarget;
+    public Vector2 textDescObstaclesTargetSize;
     public float textObstaclesFontSizeTarget;
+    public Vector2 textObstaclesTargetSize;
     public GameObject obstacles;
     public TextMeshProUGUI textObstaclesDesc;
     public TextMeshProUGUI textObstacles;
@@ -44,7 +51,9 @@ public class WriteData : MonoBehaviour
     public Vector2 timeTargetSize;
     public float timeTargetRotation;
     public float textTimeDescFontSizeTarget;
+    public Vector2 textDescTimeTargetSize;
     public float textTimeFontSizeTarget;
+    public Vector2 textTimeTargetSize;
     public GameObject time;
     public TextMeshProUGUI textTimeDesc;
     public TextMeshProUGUI textTime;
@@ -54,59 +63,57 @@ public class WriteData : MonoBehaviour
     public Vector2 distanceTargetSize;
     public float distanceTargetRotation;
     public float textDistanceDescFontSizeTarget;
+    public Vector2 textDescDistanceTargetSize;
     public float textDistanceFontSizeTarget;
+    public Vector2 textDistanceTargetSize;
     public GameObject distance;
     public TextMeshProUGUI textDistanceDesc;
     public TextMeshProUGUI textDistance;
-
     private string defaultTextObstacles = " OBSTACLES";
+    private string defaultTextTime = "\nSEC";
+    private string defaultTextDistance = " Mètres";
     void Start()
     {
-        // textScore.SetText(DataStorage.instance.score.ToString());
-        // textCalories.SetText(DataStorage.instance.burnedCalories.ToString());
-        // textObstacles.SetText(DataStorage.instance.dodgedObstacle.ToString() + defaultTextObstacles);
-        // TEST
-        textScore.SetText("123");
-        textCalories.SetText("321");
-        textObstacles.SetText("12" + defaultTextObstacles);
+        textCalories.SetText(DataStorage.instance.burnedCalories.ToString());
+        textScore.SetText(DataStorage.instance.score.ToString());
+        textObstacles.SetText(DataStorage.instance.dodgedObstacle.ToString() + defaultTextObstacles);
+        textTime.SetText(DataStorage.instance.time.ToString() + defaultTextTime);
+        textDistance.SetText(DataStorage.instance.distance.ToString() + defaultTextDistance);
         StartCoroutine(StartDataAnimation());
     }
 
     IEnumerator StartDataAnimation()
     {
-        float waiting = 0.8f;
-        yield return new WaitForSeconds(waiting);
-
         calories.SetActive(true);
-        yield return new WaitForSeconds(waiting);
-        StartCoroutine(StartObjectAnimation(calories, textCaloriesDesc, textCalories, caloriesTargetPosition, caloriesTargetSize, caloriesTargetRotation, textCaloriesDescFontSizeTarget, textCaloriesFontSizeTarget));
+        yield return new WaitForSeconds(timePrinting);
+        StartCoroutine(StartObjectAnimation(calories, textCaloriesDesc, textCalories, caloriesTargetPosition, caloriesTargetSize, caloriesTargetRotation, textCaloriesDescFontSizeTarget, textCaloriesFontSizeTarget, textDescCaloriesTargetSize, textCaloriesTargetSize));
 
         yield return new WaitForSeconds(timeAnimation);
 
         score.SetActive(true);
-        yield return new WaitForSeconds(waiting);
-        StartCoroutine(StartObjectAnimation(score, textScoreDesc, textScore, scoreTargetPosition, scoreTargetSize, scoreTargetRotation, textScoreDescFontSizeTarget, textScoreFontSizeTarget));
+        yield return new WaitForSeconds(timePrinting);
+        StartCoroutine(StartObjectAnimation(score, textScoreDesc, textScore, scoreTargetPosition, scoreTargetSize, scoreTargetRotation, textScoreDescFontSizeTarget, textScoreFontSizeTarget, textDescScoreTargetSize, textScoreTargetSize));
 
         yield return new WaitForSeconds(timeAnimation);
 
         obstacles.SetActive(true);
-        yield return new WaitForSeconds(waiting);
-        StartCoroutine(StartObjectAnimation(obstacles, textObstaclesDesc, textObstacles, obstaclesTargetPosition, obstaclesTargetSize, obstaclesTargetRotation, textObstaclesDescFontSizeTarget, textObstaclesFontSizeTarget));
+        yield return new WaitForSeconds(timePrinting);
+        StartCoroutine(StartObjectAnimation(obstacles, textObstaclesDesc, textObstacles, obstaclesTargetPosition, obstaclesTargetSize, obstaclesTargetRotation, textObstaclesDescFontSizeTarget, textObstaclesFontSizeTarget, textDescObstaclesTargetSize, textObstaclesTargetSize));
         
         yield return new WaitForSeconds(timeAnimation);
 
         time.SetActive(true);
-        yield return new WaitForSeconds(waiting);
-        StartCoroutine(StartObjectAnimation(time, textTimeDesc, textTime, timeTargetPosition, timeTargetSize, timeTargetRotation, textTimeDescFontSizeTarget, textTimeFontSizeTarget));
+        yield return new WaitForSeconds(timePrinting);
+        StartCoroutine(StartObjectAnimation(time, textTimeDesc, textTime, timeTargetPosition, timeTargetSize, timeTargetRotation, textTimeDescFontSizeTarget, textTimeFontSizeTarget, textDescTimeTargetSize, textTimeTargetSize));
         
         yield return new WaitForSeconds(timeAnimation);
 
         distance.SetActive(true);
-        yield return new WaitForSeconds(waiting);
-        StartCoroutine(StartObjectAnimation(distance, textDistanceDesc, textDistance, distanceTargetPosition, distanceTargetSize, distanceTargetRotation, textDistanceDescFontSizeTarget, textDistanceFontSizeTarget));
+        yield return new WaitForSeconds(timePrinting);
+        StartCoroutine(StartObjectAnimation(distance, textDistanceDesc, textDistance, distanceTargetPosition, distanceTargetSize, distanceTargetRotation, textDistanceDescFontSizeTarget, textDistanceFontSizeTarget, textDescDistanceTargetSize, textDistanceTargetSize));
     }
 
-    IEnumerator StartObjectAnimation(GameObject gameObject, TextMeshProUGUI textDesc, TextMeshProUGUI text, Vector2 targetPosition, Vector2 targetSize, float targetRotation, float descFontSizeTarget, float fontSizeTarget)
+    IEnumerator StartObjectAnimation(GameObject gameObject, TextMeshProUGUI textDesc, TextMeshProUGUI text, Vector2 targetPosition, Vector2 targetSize, float targetRotation, float descFontSizeTarget, float fontSizeTarget, Vector2 textDescTargetSize, Vector2 textTargetSize)
     {
         float t = 0f;
 
@@ -115,6 +122,8 @@ public class WriteData : MonoBehaviour
         Quaternion initialRotation = gameObject.GetComponent<RectTransform>().rotation;
         float initialDescFontSize = textDesc.fontSize;
         float initialFontSize = text.fontSize;
+        Vector2 textDescSizeInitialSize = textDesc.gameObject.GetComponent<RectTransform>().sizeDelta;
+        Vector2 textSizeInitialSize = text.gameObject.GetComponent<RectTransform>().sizeDelta;
 
         while (t < timeAnimation)
         {
@@ -127,6 +136,8 @@ public class WriteData : MonoBehaviour
             gameObject.GetComponent<RectTransform>().rotation = Quaternion.Euler(0f, 0f, Mathf.LerpAngle(initialRotation.eulerAngles.z, targetRotation, smoothStep));
             textDesc.fontSize = Mathf.Lerp(initialDescFontSize, descFontSizeTarget, smoothStep);
             text.fontSize = Mathf.Lerp(initialFontSize, fontSizeTarget, smoothStep);
+            textDesc.gameObject.GetComponent<RectTransform>().sizeDelta = Vector2.Lerp(textDescSizeInitialSize, textDescTargetSize, smoothStep);
+            text.gameObject.GetComponent<RectTransform>().sizeDelta = Vector2.Lerp(textSizeInitialSize, textTargetSize, smoothStep);
 
             yield return null;
         }
