986f380a0f17cf43e8f3bd8eec5d82125f229f99
Nay <ilan.outyey@gmail.com>
2023-12-07 17:43:46 +0100

    Fading introduction

--- Assets/Scripts/Manager/Introduction.cs
+++ Assets/Scripts/Manager/Introduction.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Introduction : MonoBehaviour
@@ -11,11 +12,11 @@ public class Introduction : MonoBehaviour
     public GameObject textNootNoot;
     public GameObject logo;
 
-
     public float showTextDelay = 2.5f; // Délai avant d'afficher le texte
     public float hideTextDelay = 3.4f; // Délai avant de cacher le texte
     public float showLogoDelay = 5.9f; // Délai avant d'afficher le logo
     public float hideLogoDelay = 5f; // Délai avant de cacher le logo
+    public float fadeOutDelay = 8.5f; // Délai avant de débuter le fondu
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +27,7 @@ public class Introduction : MonoBehaviour
         StartCoroutine(PlayIntroSound());
         StartCoroutine(ShowAndHideText());
         StartCoroutine(ShowAndHideLogo());
-        
+        StartCoroutine(FadeOut());
     }
 
     // Coroutine pour jouer le son avec une petite pause
@@ -56,15 +57,35 @@ public class Introduction : MonoBehaviour
         yield return new WaitForSeconds(showLogoDelay); // Attends avant d'afficher le logo
         logo.SetActive(true);
 
-        StartCoroutine(startLogoAnimation());
+        StartCoroutine(StartLogoAnimation());
 
         yield return new WaitForSeconds(hideLogoDelay); // Attends avant de cacher le logo
         logo.SetActive(false);
+    }
+
+    // Coroutine pour le fondu (fade) vers l'opacité 0
+    IEnumerator FadeOut()
+    {
+        yield return new WaitForSeconds(hideLogoDelay + fadeOutDelay); // Attends avant de débuter le fondu
+
+        Image logoImage = logo.GetComponent<Image>();
+        Color logoColor = logoImage.color;
+
+        float t = 0f;
+        while (t < fadeOutDelay)
+        {
+            t += Time.deltaTime;
+
+            float alpha = Mathf.Lerp(1f, 0f, t / fadeOutDelay);
+            logoImage.color = new Color(logoColor.r, logoColor.g, logoColor.b, alpha);
+            yield return null;
+        }
 
         SceneManager.LoadScene("MainMenu");
     }
 
-    IEnumerator startLogoAnimation()
+    // Coroutine pour l'animation du logo (à compléter si nécessaire)
+    IEnumerator StartLogoAnimation()
     {
         Vector2 initialSize = logo.GetComponent<RectTransform>().sizeDelta;
         Vector2 targetSize = new Vector2(905.2627f, 460.1752f);
